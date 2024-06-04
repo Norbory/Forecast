0c2db5b36d49608255a323d8246741f6cffd213b
Norbory <mandros.angelof@pucp.edu.pe>
2024-06-03 20:38:36 -0500

    Created pages and Added routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from './shared/components/footer/footer.component';
-import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from "./shared/components/footer/footer.component";
+import { NavbarComponent } from "./shared/components/navbar/navbar.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, FooterComponent, NavbarComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, FooterComponent, NavbarComponent]
 })
 export class AppComponent {
   title = 'Forecast';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
+        path: '',
+        loadChildren: () => import('./home/home.routes').then(m => m.HOME_ROUTES)
+    },
+    {
         path: 'weather',
         loadChildren: () => import('./pages/pages.routes').then(m => m.PAGES_ROUTES)
     },
