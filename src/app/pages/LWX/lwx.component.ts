b478d5e49ca3e8b641392939b09523c854e26069
Norbory <mandros.angelof@pucp.edu.pe>
2024-06-03 21:50:15 -0500

    Forecast page for Columbia

--- src/app/pages/LWX/lwx.component.ts
+++ src/app/pages/LWX/lwx.component.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Chart, registerables } from 'chart.js';
 
 @Component({
   selector: 'app-lwx',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './lwx.component.html',
   styleUrl: './lwx.component.css'
 })
-export class LWXComponent {
 
+export class LWXComponent implements OnInit {
+  constructor() {
+    Chart.register(...registerables);
+  }
+
+  ngOnInit(): void {
+    this.createChart();
+  }
+
+  createChart(): void {
+    const ctx = document.getElementById('lineChart') as HTMLCanvasElement;
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+          {
+            label: 'Dew Point',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            borderColor: 'rgba(255, 99, 132, 1)',
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            fill: false,
+          },
+          {
+            label: 'Relative Humidity',
+            data: [28, 48, 40, 19, 86, 27, 90],
+            borderColor: 'rgba(54, 162, 235, 1)',
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            fill: false,
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          x: {
+            beginAtZero: true
+          },
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
 }
